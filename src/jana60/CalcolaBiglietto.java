cc3cff6c0b030f871100ce89d6932d84f6933ba1
paololabriola <paololabriola02@gmail.com>
2022-06-07 16:33:09 +0200

    Exercise fixed

--- src/jana60/CalcolaBiglietto.java
+++ src/jana60/CalcolaBiglietto.java
@@ -12,20 +12,22 @@ public class CalcolaBiglietto {
 		
 		System.out.print("Inserisci il numero di chilometri da percorrere: ");
 		chilometriDaPercorrere = scan.nextFloat();
-		System.out.println("Inserisci l'età del passeggero: ");
+		System.out.print("Inserisci l'età del passeggero: ");
 		etaPasseggero = scan.nextInt();
 		
 		float prezzoBiglietto = chilometriDaPercorrere * 0.21f;
+		float scontoMinorenni = (prezzoBiglietto * 20) / 100;
+		float scontoOver65 = (prezzoBiglietto * 40) / 100;
 		
 		if(etaPasseggero < 18) {
-			prezzoBigliettoScontato = (prezzoBiglietto * 20) / 100;
-		}else if(etaPasseggero>65){
-			prezzoBigliettoScontato = (prezzoBiglietto * 40) /100;
+			prezzoBigliettoScontato = prezzoBiglietto - scontoMinorenni;
+		}else if(etaPasseggero > 65){
+			prezzoBigliettoScontato = prezzoBiglietto - scontoOver65;
 		}else {
 			prezzoBigliettoScontato = prezzoBiglietto;
 		}
 		
-		System.out.println("Il prezzo finale del biglietto è di: " + prezzoBigliettoScontato);
+		System.out.println("Il prezzo finale del biglietto scontato è di: " + prezzoBigliettoScontato + "€");
 		
 		scan.close();
 		
